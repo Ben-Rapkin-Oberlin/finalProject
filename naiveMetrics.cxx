73bad121c87a3a6ff98b4c1f01d31f2d9d17041b
1pasword1 <34221091+1pasword1@users.noreply.github.com>
2022-05-20 11:51:40 -0400

    I have updated the varNames function to now be able to detect 10 naming conventions and add them to a global vector for future refrence. It has not been tested and I still need to implement logic for multiple varible on the same line, ie "int a,b;" or, even worse, "int a_A, b-b;"

--- naiveMetrics.cxx
+++ naiveMetrics.cxx
@@ -3,200 +3,278 @@
 #include <cctype>
 
 #include <iostream>
-#include <fstream> 
+#include <fstream>
 
 using namespace std;
 
+// TODO
+// implement knownNames
+// test functions
+
+vector<string> knownNames;
 
 
-//TODO
-//implement knownNames
-//test functions
-
-vector <string> knownNames;
-
-//should modify to store name at end using var
-//should change to return int, can use that as a vector index in main
-int varNames(string line){ //right now plan to store in profile
-    //strip start
-    size_t offset=0;
+//TODO consider multiple declarations per line
+int varNames(string line)
+{ // right now plan to store in profile
+    // strip start
+    size_t offset = 0;
     size_t startI;
     size_t endI;
-    int names;
-    //vscode changes tab to space
-    char tab = char(9);
-    int a=tabSpace(line);
 
-    //assumes tabs will only be used at the start of a line
-    if (a==1){//there are tabs and we are removing them
-        while(line.find(tab)!=string::npos){
-            line=line.substr(line.find(tab)+1);  
+    int names;
+
+    bool dash = false;
+    bool underScore = false;
+    // vscode changes tab to space
+    char tab = char(9);
+    int a = tabSpace(line);
+
+    // assumes tabs will only be used at the start of a line
+    if (a == 1)
+    { // there are tabs and we are removing them
+        while (line.find(tab) != string::npos)
+        {
+            line = line.substr(line.find(tab) + 1);
         }
     }
-    if (a==2){
-        while (!isalpha(line.at(offset))){
+    if (a == 2)
+    {
+        while (!isalpha(line.at(offset)))
+        {
             offset++;
         }
     }
 
-    //TODO
-    //check for multiple varibles: int a,b =0;
-    /*if (line.find(",")!=string::npos){ 
+    // TODO
+    // check for multiple varibles: int a,b =0;
+    /*if (line.find(",")!=string::npos){
     }
     */
 
-   size_t Eindex = line.find("=");
-    if (Eindex!=string::npos && line.find("==")==string::npos && line.find("===")==string::npos){
-        if (offset>=Eindex){
-            offset=0;
+    size_t Eindex = line.find("=");
+    if (Eindex != string::npos && line.find("==") == string::npos && line.find("===") == string::npos)
+    {
+        if (offset >= Eindex)
+        {
+            offset = 0;
         }
-        
-        string var = line.substr(offset+1, Eindex-offset);
-        //at this point, var is of form int a_a, A_A, a-a, aa, AA, Aa
+        string var = line.substr(offset + 1, Eindex - offset);
+        // at this point, var is of form int a_a, A_A, a-a, aa, AA, Aa
+        // so now we trim it to "a_a" and add it to knownNames
+        endI = var.length() - 1;
 
-            if (var.find("_")!=string::npos){
-                //snake_Case,
-                size_t Uindex=var.find("_");      
-                if (isupper(var.at(Uindex+1))){
-                    //upper or snake
-                    startI=Uindex-1;
-                    endI=Uindex+1;
-                    if (isupper(var.at(Uindex-1))){
-                        //upper
-                        names[0]=names[0]+1;
-                    }
-                    else {
-                        //snake
-                        names[1]=names[1]+1;
-                    }
-                    knownNames.push_back(var.substr(startI,endI-startI+1));
-                }
-                else {
-                    //lower, assuming no odD_case
-                    names[2]=names[2]+1;
-                    endI=Eindex;
-                    startI=offset;
-                    while(!isalpha(var.at(endI))){
-                        endI--;
-                    }
-                    while (isalpha(var.at(startI+1))){
-                        startI++;
-                    }
-                    knownNames.push_back(var.substr(startI,endI-startI+1));
-                }
-            }
-            else if (var.find("-")!=string::npos){
-                //- means kabab-case (- overides capitilization)
-                names[3]=names[3]+1;
-            }
-            else {
-                int capCount=0;
-                for (int i=0; i<(int) var.size(); i++) {
-                    if (isupper(var.at(i))){
-                        capCount++;
-                    }
-                }
-                if (capCount==1){
-                    //camel case
-                    names[4]=names[4]+1;
-                }
-                if (capCount==2){
-                    //capital case
-                    names[5]=names[5]+1;
-                }
-                else {
-                    //flat or single word, undescriptive
-                    names[6]=names[6]+1;
-                }
-            }
-        endI=var.length();
-        startI=0;
-
-        while(!isalpha(var.at(endI))){
+        while (!isalpha(var.at(endI)))
+        {
             endI--;
         }
-        while (!isalpha(var.at(startI+1))){
-            startI++;
+        startI = endI;
+        bool a = true;
+        // consider a-a or a_a
+        while (a)
+        {
+            if (!isalpha(var.at(startI - 1)))
+            {
+                // we are at either index 4 in "int aa" or index 6 in "int a_a"
+                if (var.at(startI - 1) == '-')
+                {
+                    startI--;
+                    dash = true;
+                }
+                else if (var.at(startI - 1) == '_')
+                {
+                    startI--;
+                    underScore = true;
+                }
+
+                else
+                {
+                    a = false;
+                }
+            }
+            else
+            {
+                startI--;
+            }
         }
-        //call profile to store prevelance
-        float sum=0;
-        for (int i=0; i<7; i++){
-            sum+=names[i];
+        var = var.substr(startI, endI - startI + 1);
+    
+        //if var is already in knowNames it's naming convention has been recorded
+        //Thus we return as we don't want to count "int a;" and "a=3;" as seperate
+        //varibles
+        if (std::find(knownNames.begin(), knownNames.end(), var) == knownNames.end()){
+            return 0;
         }
-        for (int i=0; i<7; i++){
-            names[i]=names[1]/sum;
+        
+        knownNames.push_back(var);
+        if (underScore)
+        {
+            // snake_Case,
+            size_t Uindex = var.find("_");
+            if (isupper(var.at(Uindex + 1)))
+            {
+                // upper or snake
+                if (isupper(var.at(Uindex - 1)))
+                {
+                    // upper A_A
+                    names = 1;
+                }
+                else
+                {
+                    // snake a_A
+                    names = 2;
+                }
+            }
+            else
+            {
+                // lower a_a
+                // assuming no odD_case
+                names = 3;
+            }
         }
+        else if (dash)
+        {
+            //- means kabab-case
+            size_t Dindex = var.find("-");
+            if (isupper(var.at(Dindex + 1)))
+            {
+                // upper A-A or a-A
+                if (isupper(var.at(Dindex - 1)))
+                {
+                    // upper A-A
+                    names = 4;
+                }
+                else
+                {
+                    // snake a-A
+                    names = 5;
+                }
+            }
+            else
+            {
+                // lower a-a
+                // assuming no odD_case
+                names = 6;
+            }
+        }
+        else
+        {
+            int capCount = 0;
+            for (int i = 0; i < (int)var.size(); i++)
+            {
+                if (isupper(var.at(i)))
+                {
+                    capCount++;
+                }
+            }
+            if (capCount == 1)
+            {
+                // camelCase
+                names = 7;
+            }
+            else if (capCount == 2)
+            {
+                // CapitalCase
+                names = 8;
+            }
+            else if (capCount > 2)
+            {
+                // all caps
+                names = 9;
+            }
+            else
+            {
+                // flat or single word, undescriptive
+                names = 10;
+            }
+        }
+        // call profile to store prevelance
     }
     return names;
-} 
+}
 
-//Tab vs Spaces
-//tested and working
-int tabSpace(string line){
-    //assume it will be first char
-    //0 for error/niether, 1 for tab, 2 for space
-    char tab=char(9);
+// Tab vs Spaces
+// tested and working
+int tabSpace(string line)
+{
+    // assume it will be first char
+    // 0 for error/niether, 1 for tab, 2 for space
+    char tab = char(9);
     try
     {
-        if (line.at(0)==tab){
-         return 1;
+        if (line.at(0) == tab)
+        {
+            return 1;
         }
-        else if (line.at(0)==' '){
+        else if (line.at(0) == ' ')
+        {
             return 2;
         }
-        else{
+        else
+        {
             return 0;
         }
     }
 
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         return 0;
     }
 }
 
-// ' = ' vs '=' 
-//tested and working
-int equalSpacing(string line){
-    //return 1 for " = " and 2 for "=" and 0 for none
-    //assume at most one assigment (=) per line and not = && == on same line
+// ' = ' vs '='
+// tested and working
+int equalSpacing(string line)
+{
+    // return 1 for " = " and 2 for "=" and 0 for none
+    // assume at most one assigment (=) per line and not = && == on same line
     size_t Eindex = line.find("=");
-    if (Eindex!=string::npos && line.find("==")==string::npos 
-    && line.find("===")==string::npos){
-        if (line.at(Eindex-1)==' ' && line.at(Eindex+1)==' '){
+    if (Eindex != string::npos && line.find("==") == string::npos && line.find("===") == string::npos)
+    {
+        if (line.at(Eindex - 1) == ' ' && line.at(Eindex + 1) == ' ')
+        {
             return 1;
         }
-        else{
+        else
+        {
             return 2;
         }
     }
     return 0;
 }
-    
-//Empty Lines
-//tested and working
-int emptyLine(string line){
-    // allow ' ' '\n' tab 
-    for (int i=33; i<127; i++){
-        if (line.find(char(i))!=string::npos){
+
+// Empty Lines
+// tested and working
+int emptyLine(string line)
+{
+    // allow ' ' '\n' tab
+    for (int i = 33; i < 127; i++)
+    {
+        if (line.find(char(i)) != string::npos)
+        {
             return 0;
         }
     }
     return 1;
 }
 
-//used for nullform, checks for if statment and if that statment does not use logical opperators
-//fairly simple, should add in more complexity
-//things like a==NULL vs !a
-int boolif(string line){
+// used for nullform, checks for if statment and if that statment does not use logical opperators
+// fairly simple, should add in more complexity
+// things like a==NULL vs !a
+int boolif(string line)
+{
     // 0 for not applicable if or line, 1 for applicable if but not null, 2 for applicable if and null
-    if  (line.find("if")!=string::npos){
-        if (line.find("!=NULL")!=string::npos){
+    if (line.find("if") != string::npos)
+    {
+        if (line.find("!=NULL") != string::npos)
+        {
             return 2;
         }
-        for (auto & element : knownNames) {
+        for (auto &element : knownNames)
+        {
             string temp = "(" + element + ")";
-            if (line.find(temp)!=string::npos){
+            if (line.find(temp) != string::npos)
+            {
                 return 1;
             }
         }
@@ -204,41 +282,49 @@ int boolif(string line){
     }
 }
 
-//not done // ++ vs +=1 vs a=a+1
-//substr is (pos,len from pos, including pos) will need to rewrite code
-vector<int> increment (string line){
+// not done // ++ vs +=1 vs a=a+1
+// substr is (pos,len from pos, including pos) will need to rewrite code
+vector<int> increment(string line)
+{
     //++ -- += -= these corraspond to vector indexes
-    vector<int> a={0,0,0,0}; 
+    vector<int> a = {0, 0, 0, 0};
     // a line can have multiple inc and dec
-    string temp=line;
-    while (temp.find("++")){
-        a[0]=a[0]+1;
-        temp=temp.substr(temp.find("++"),temp.length());        
+    string temp = line;
+    while (temp.find("++"))
+    {
+        a[0] = a[0] + 1;
+        temp = temp.substr(temp.find("++"), temp.length());
     }
-    temp=line;
-    while (temp.find("--")){
-        a[1]=a[1]+1;
-        temp=temp.substr(temp.find("--"),temp.length());        
+    temp = line;
+    while (temp.find("--"))
+    {
+        a[1] = a[1] + 1;
+        temp = temp.substr(temp.find("--"), temp.length());
     }
-    temp=line;
-    while (temp.find("+=")){
-        a[2]=a[2]+1;
-        temp=temp.substr(temp.find("+="),temp.length());        
+    temp = line;
+    while (temp.find("+="))
+    {
+        a[2] = a[2] + 1;
+        temp = temp.substr(temp.find("+="), temp.length());
     }
-    temp=line;
-    while (temp.find("-+")){
-        a[3]=a[3]+1;
-        temp=temp.substr(temp.find("-="),temp.length());        
+    temp = line;
+    while (temp.find("-+"))
+    {
+        a[3] = a[3] + 1;
+        temp = temp.substr(temp.find("-="), temp.length());
     }
     return a;
 }
 
-// \n after . 
-int dotSpace(string line){
-    char tab= char (9);
-    size_t dotIndex=line.find(".");
-    if (dotIndex!=string::npos){
-        if (line.at(dotIndex-1)==' ' ||line.at(dotIndex-1)==tab){
+// \n after .
+int dotSpace(string line)
+{
+    char tab = char(9);
+    size_t dotIndex = line.find(".");
+    if (dotIndex != string::npos)
+    {
+        if (line.at(dotIndex - 1) == ' ' || line.at(dotIndex - 1) == tab)
+        {
             return 1;
         }
     }
