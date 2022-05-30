841440e64bc052131ac01999fafe384a6bbb0784
Hikari <kmertens@oberlin.edu>
2022-05-29 21:21:04 -0400

    changed to output pair & syntax fixes

--- dataAnalysis.cxx
+++ dataAnalysis.cxx
@@ -1,56 +1,13 @@
 #include <vector>
 #include <filesystem>
 #include <string>
-#include <any>
+#include <utility>
 #include <random>
 #include <array>
 #include <cmath>
 #include "train.cxx"
 
 using namespace std;
-//add clean() for memory allocated, use vector destuctor
-
-std::array<float, 10>bootstrap();
-
-
-std::array<std::vector<std::any>, 2> dataAnalysis(string path, int bootCount) {
-	std::vector<vector<std::array<float, 20> > > pData;
-	std::vector<string> pNames; //index of name list correlates to index in pData with that person's data
-
-
-
-
-	//loops through main directory of people
-	for (const auto& person : directory_iterator(path)) {
-		pNames.push_back(person); //adds to name list
-		std::vector<std::array<float, 10> > tempData; //create temp vector for person's code
-
-
-
-
-		//loops through a person's code directory
-		for (const auto& file : directory_iterator(person.path()) {
-			tempData.push_back(train(file.path()));
-		}
-
-		
-		pData.push_back(bootstrap(tempData, bootCount));
-
-
-		//cleans up
-		tempData.~vector();
-	}
-
-	//array for return
-	std::array < std::vector<std::any>, 2> fArray;
-	fArray[0] = pNames;
-	fArray[1] = pData;
-	
-	return fArray;
-
-}
-
-
 
 
 
@@ -68,7 +25,7 @@ std::array<float, 20>bootstrap(vector<std::array<float, 10> > data, int bCount) 
 	std::vector<float * > bData; //create temp vector for bootstrap data, stores pointers to float arrays
 
 	//creates new data samples equal to bCount, stores in bData
-	for (int i = 0; i < bCount; ++i;) {
+	for (int i = 0; i < bCount; ++i) {
 
 		//creates new dynamically allocated array stored in vector at index i
 		bData.push_back(new float[10]{
@@ -92,12 +49,12 @@ std::array<float, 20>bootstrap(vector<std::array<float, 10> > data, int bCount) 
 	//stores standard devation and mean for function return
 	std::array<float, 20> fData;
 
-	for (int i = 0; i < 10; ++i;) {
+	for (int i = 0; i < 10; ++i) {
 
 		tempSum = 0;
 
 		//loops through each sample to add for mean
-		for (int x = 0; x < bCount; ++x;) {
+		for (int x = 0; x < bCount; ++x) {
 			tempSum += bData.at(x)[i];
 		}
 		
@@ -109,7 +66,7 @@ std::array<float, 20>bootstrap(vector<std::array<float, 10> > data, int bCount) 
 		tempSum = 0;
 
 		//loops through each sample to add for SD according to formula for standard deviation
-		for (int x = 0; x < bCount; ++x;) {
+		for (int x = 0; x < bCount; ++x) {
 			tempSum = tempSum + std::pow(std::abs(bData.at(x)[i]/*value*/ - fData[(i + 1) * 2] /*mean*/), 2);
 		}
 		//finishes calulating SD and places it in correct position in fData
@@ -117,10 +74,47 @@ std::array<float, 20>bootstrap(vector<std::array<float, 10> > data, int bCount) 
 	}
 
 	//cleans up
-	for (int x = 0; x < bCount; ++x;) {
+	for (int x = 0; x < bCount; ++x) {
 		delete[] bData.at(x);
 	}
 	bData.~vector();
 
 	return fData;
+}
+
+std::pair< std::vector<string>, std::vector<std::array<float, 20> > > dataAnalysis(string path, int bootCount) {
+	std::vector<std::array<float, 20> > pData;
+	std::vector<string> pNames; //index of name list correlates to index in pData with that person's data
+
+
+
+
+	//loops through main directory of people
+	for (const auto& person : std::filesystem::directory_iterator(path)) {
+		pNames.push_back(person.path().stem().string()); //adds to name list
+		std::vector<std::array<float, 10> > tempData; //create temp vector for person's code
+
+
+
+
+		//loops through a person's code directory
+		for (const auto& file : std::filesystem::directory_iterator(person.path())) {
+			tempData.push_back(train(file.path().string()));
+		}
+
+
+		pData.push_back(bootstrap(tempData, bootCount));
+
+
+		//cleans up
+		tempData.~vector();
+	}
+
+	//array for return
+	std::pair< std::vector<string>, std::vector<std::array<float, 20> > > fPair;
+	fPair.first = pNames;
+	fPair.second = pData;
+
+	return fPair;
+
 }
